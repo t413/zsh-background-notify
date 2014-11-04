d9e6cef4611eeb680e6048c8833974c0f40fe255
Tim O'Brien <tobrien@hadal.com>
2014-11-04 14:11:50 -0800

    Working under ubuntu, needs osx testing

--- README.md
+++ README.md
@@ -2,3 +2,15 @@ zsh-background-notify
 =====================
 
 cross-platform background notifications for long running commands!
+
+Supports OSX and ubuntu linux.
+
+
+## Alternatives:
+
+Here are a few similar alternatives, most are platform spesific however.
+
+- This [reddit post](http://www.reddit.com/r/linux/comments/1pooe6/zsh_tip_notify_after_long_processes/)
+- [zsh-notify](https://github.com/marzocchi/zsh-notify) plugin for Mac OS X
+- [dotzsh notify](https://github.com/dotphiles/dotzsh/tree/master/modules/notify)
+- [zbell](https://gist.github.com/jpouellet/5278239)
---
+++ bgnotify.plugin.zsh
@@ -0,0 +1,54 @@
+#!/usr/bin/env zsh
+
+
+## setup ##
+
+[[ -o interactive ]] || return #interactive only!
+zmodload zsh/datetime || { print "can't load zsh/datetime"; return } # faster than date()
+autoload -Uz add-zsh-hook || { print "can't add zsh hook!"; return }
+
+(( ${+bgnotify_threshold} )) || bgnotify_threshold=5 #default 10 seconds
+
+bgnotify_timestamp=$EPOCHSECONDS
+
+
+
+## definitions ##
+
+currentWindowId () {
+  if hash xprop 2>/dev/null; then #ubuntu!
+    xprop -root | awk '/NET_ACTIVE_WINDOW/ { print $5; exit }'
+  elif hash osascript 2>/dev/null; then #osx
+    osascript -e 'tell application (path to frontmost application as text) to id of front window' 2&> /dev/null
+  fi
+}
+
+bgnotify () {
+  if hash notify-send 2>/dev/null; then #ubuntu!
+    notify-send $1 $2
+  elif hash terminal-notifier 2>/dev/null; then #osx
+    terminal-notifier -message $1 -title $2
+  elif hash growlnotify 2>/dev/null; then #osx growl
+    growlnotify -m $1 $2
+  fi
+}
+
+bgnotify_begin() {
+	bgnotify_timestamp=$EPOCHSECONDS
+	bgnotify_lastcmd=$1
+  bgnotify_windowid=$(currentWindowId)
+}
+
+bgnotify_end() {
+  didexit=$?
+	past_threshold=$(( $EPOCHSECONDS - $bgnotify_timestamp >= $bgnotify_threshold ))
+  is_background=$([[ "$(currentWindowId)" -eq "$bgnotify_windowid" ]])
+
+	if (( ! is_background )) && (( past_threshold )); then
+		print -n "\a"
+    bgnotify $([ $didexit -ne 0 ] && echo '#fail' || echo '#win!') "$bgnotify_lastcmd"
+	fi
+}
+
+add-zsh-hook preexec bgnotify_begin
+add-zsh-hook precmd bgnotify_end
